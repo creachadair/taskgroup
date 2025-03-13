52b6f245bfaacccd0fb2a6a7500015e1154b7aff
M. J. Fromberger <michael.j.fromberger@gmail.com>
2025-03-13 16:13:51 -0700

    taskgroup: remove the deprecated NoError wrapper

--- taskgroup.go
+++ taskgroup.go
@@ -178,11 +178,6 @@ func adaptErrorFunc(ef any) errorFunc {
 	}
 }
 
-// NoError adapts f to a Task that executes f and reports a nil error.
-//
-// Deprecated: Use [Group.Run] or [taskgroup.Run] instead.
-func NoError(f func()) Task { return func() error { f(); return nil } }
-
 func noError(f func()) Task { return func() error { f(); return nil } }
 
 // Limit returns g and a [StartFunc] that starts each task passed to it in g,
